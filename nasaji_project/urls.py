3082ecf0d0fa1a0601005e80db8a9009b0a4ed3a
amir <amir@gmail.com>
2024-12-17 11:45:18 +0330

    test

--- nasaji_project/urls.py
+++ nasaji_project/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-path('', include('home.urls', namespace='home')),
+    path('', include('home.urls', namespace='home')),
+    path('about_us/', include('about.urls', namespace='about'))
 ]
