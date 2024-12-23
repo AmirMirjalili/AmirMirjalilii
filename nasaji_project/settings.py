67f56399e0d3e59cbeff9e5fbd81a65a8d85d701
amir <amir@gmail.com>
2024-12-23 10:52:02 +0330

    test

--- nasaji_project/settings.py
+++ nasaji_project/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'account',
     'about',
     'django.contrib.humanize',
+    'mptt',
 ]
 
 MIDDLEWARE = [
@@ -72,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'product.context_processors.categories_context_processor',
             ],
         },
     },
--- product/urls.py
+++ product/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'details_view'
 urlpatterns = [
     path('product/<slug:slug>/', views.product_detail, name='product_details'),
+    path('category/<int:category_id>/', views.category_detail, name='category'),
 ]
